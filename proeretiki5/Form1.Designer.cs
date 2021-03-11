05579c1fcaef87d22dc72321d7b783740c686200
zartilas <36131492+zartilas@users.noreply.github.com>
2021-03-11 18:43:03 +0200

    Project Commit

---
+++ proeretiki5/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace proeretiki5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.ForeColor = System.Drawing.Color.Green;
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Black;
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.ForeColor = System.Drawing.Color.Green;
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Black;
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.ForeColor = System.Drawing.Color.Green;
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Black;
+            resources.ApplyResources(this.button4, "button4");
+            this.button4.ForeColor = System.Drawing.Color.Green;
+            this.button4.Name = "button4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Black;
+            resources.ApplyResources(this.button5, "button5");
+            this.button5.ForeColor = System.Drawing.Color.Green;
+            this.button5.Name = "button5";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Black;
+            resources.ApplyResources(this.button6, "button6");
+            this.button6.ForeColor = System.Drawing.Color.Green;
+            this.button6.Name = "button6";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Black;
+            resources.ApplyResources(this.button7, "button7");
+            this.button7.ForeColor = System.Drawing.Color.Green;
+            this.button7.Name = "button7";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Name = "label1";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ proeretiki5/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proeretiki5
+{
+    
+    public partial class Form1 : Form
+    {
+        long count;
+        public Stopwatch watch = new Stopwatch();
+        string v = "Χρειάζεται για να υπολογίσει" ;
+        string s = " seconds!";
+        double devil = 666;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            watch.Start();
+            for (int i = 0; i < int.MaxValue; i++)
+                count++;
+            watch.Stop();
+            MessageBox.Show(v + " έναν μεγάλο θετικό αριθμό, " + watch.Elapsed.TotalSeconds.ToString() + s);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            watch.Start();
+            for (int i = 0; i > int.MinValue; i--)
+                count--;
+            watch.Stop();
+            MessageBox.Show(v + " έναν μεγάλο αρνητικό αριθμό, " + watch.Elapsed.TotalSeconds.ToString() + s);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            watch.Start();
+            Math.Sin(devil);
+            watch.Stop();
+            MessageBox.Show(v + " τo sin του 666, " + watch.Elapsed.TotalMilliseconds.ToString() + " milli" + s);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            watch.Start();
+            Math.Cos(devil);
+            watch.Stop();
+            MessageBox.Show(v + " το cos του 666, " +watch.Elapsed.TotalMilliseconds.ToString() + " milli" + s);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            watch.Start();
+            Math.Tan(devil);
+            watch.Stop();
+            MessageBox.Show(v + " το tan του 666, " + watch.Elapsed.TotalMilliseconds.ToString() + " milli" + s);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            watch.Start();
+            Math.Sqrt(devil);
+            watch.Stop();
+            MessageBox.Show(v + " την ρίζα του 666, " + watch.Elapsed.TotalMilliseconds.ToString() + " milli" + s);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            watch.Start();
+            Math.Pow(devil, 69);
+            watch.Stop();
+            MessageBox.Show(v + " το 666^69, " + watch.Elapsed.TotalMilliseconds.ToString() + " milli" + s);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult exit = MessageBox.Show("Exit?", "EXIT", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (exit == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            else if(exit == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+               
+            
+        }
+    }
+}
